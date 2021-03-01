107a51072796cb8fe80b70135b727440836fab22
Tommi Keskitalo <tommi.keskitalo@siili.com>
2021-03-01 16:53:56 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "Git Introduction",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};

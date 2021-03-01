af9e7fff7d488c6e9fc1d532681f67f22d9170c9
Tommi Keskitalo <tommi.keskitalo@siili.com>
2021-03-01 19:23:41 +0200

    Make secondPage prettier

--- src/pages/second.js
+++ src/pages/second.js
@@ -1,10 +1,22 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const PunainenOtsikko = styled.h1({
+  color: "red",
+});
+
+const IsoPunainenOtsikko = styled(PunainenOtsikko)({
+  fontSize: "5rem",
+});
 
 const SecondPage = () => {
   return (
     <main>
       <title>SecondPage</title>
-      <p>Nothing to see in here... Yet...</p>
+      <PunainenOtsikko>Otsikko pitäisi olla punainen</PunainenOtsikko>
+      <IsoPunainenOtsikko>Isolla otsikko</IsoPunainenOtsikko>
+      <p>Koodataan katjan kanssa</p>
+      <p>Kivaa koodata!</p>
     </main>
   );
 };

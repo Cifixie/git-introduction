fd9fe17bfec5763511c15c574c97e72c41bb44ff
Tommi Keskitalo <tommi.keskitalo@siili.com>
2021-03-01 17:53:43 +0200

    Third

---
+++ src/pages/third.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const ThirdPage = () => {
+  return (
+    <main>
+      <title>ThirdPage</title>
+      <p>Nothing to see in here... Yet...</p>
+    </main>
+  );
+};
+
+export default ThirdPage;
